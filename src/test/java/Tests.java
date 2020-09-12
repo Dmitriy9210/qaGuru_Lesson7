9e98efeecfe7fd0df0e1afa6d82167d03a3acf39
Svetlana Pavlova <>
2020-09-12 15:00:11 +0300

    add new files

---
+++ src/test/java/Tests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests {
+
+    @Test
+    void firstTest(){
+        assertTrue(false);
+    }
+
+    @Test
+    void secondTest(){
+        assertEquals(3,44);
+    }
+
+    @Test
+    void therdTest(){
+        assertNull(null);
+    }
+}
